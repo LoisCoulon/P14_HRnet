d1098db48260064d279f63a89cceaf40a548fa1f
Loïs <flojuve@hotmail.fr>
2022-06-09 12:54:03 +0200

    pagination

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -1,92 +1,44 @@
 import { useState } from "react";
+import { usePagination } from "react-table";
 import DataTable from "../../components/DataTable/DataTable";
 import Header from "../../components/Header/Header";
 import Input from "../../components/Input/Input";
+import { datas, columns } from "../../data";
+import ReactPaginate from "react-paginate";
 
 function List() {
-  const [entry, setEntry] = useState(0);
   const [search, setSearch] = useState("");
-
-  const datas = [
-    {
-      id: 1,
-      firstName: "Alex",
-      lastName: "A",
-      startDate: "12/08/2021",
-      department: "Department",
-      dateOfBirth: "31/03/2002",
-      street: "Street",
-      city: "City",
-      state: "State",
-      zipCode: "33000",
-    },
-    {
-      id: 2,
-      firstName: "Jack",
-      lastName: "B",
-      startDate: "14/07/1990",
-      department: "Department",
-      dateOfBirth: "01/01/1968",
-      street: "Street",
-      city: "City",
-      state: "State",
-      zipCode: "75000",
-    },
-    {
-      id: 3,
-      firstName: "Suzie",
-      lastName: "C",
-      startDate: "12/12/2021",
-      department: "Department",
-      dateOfBirth: "23/12/1980",
-      street: "Street",
-      city: "City",
-      state: "State",
-      zipCode: "31000",
-    },
-    {
-      id: 4,
-      firstName: "Julia",
-      lastName: "D",
-      startDate: "23/09/2020",
-      department: "Department",
-      dateOfBirth: "24/02/1998",
-      street: "Street",
-      city: "City",
-      state: "State",
-      zipCode: "56000",
-    },
-  ];
-
-  const columns = [
-    { title: "First Name", data: "firstName" },
-    { title: "Last Name", data: "lastName" },
-    { title: "Start Date", data: "startDate" },
-    { title: "Department", data: "department" },
-    { title: "Date of Birth", data: "dateOfBirth" },
-    { title: "Street", data: "street" },
-    { title: "City", data: "city" },
-    { title: "State", data: "state" },
-    { title: "Zip Code", data: "zipCode" },
-  ];
+  const [page, setPage] = useState(0);
+  const [employeesPerPage, setEmployeesPerPage] = useState(10);
+  const numberOfEmployeesVisited = page * employeesPerPage;
+  const totalPages = Math.ceil(datas.length / employeesPerPage);
 
   //search function
   const handleSearch = (event) => {
     setSearch(event.target.value);
   };
 
-  const data = datas.filter(
-    (item) =>
-      item.city.toString().toLowerCase().includes(search) ||
-      item.department.toString().toLowerCase().includes(search) ||
-      item.firstName.toString().toLowerCase().includes(search) ||
-      item.dateOfBirth.toString().toLowerCase().includes(search) ||
-      item.lastName.toString().toLowerCase().includes(search) ||
-      item.startDate.toString().toLowerCase().includes(search) ||
-      item.state.toString().toLowerCase().includes(search) ||
-      item.street.toString().toLowerCase().includes(search) ||
-      item.zipCode.toString().toLowerCase().includes(search)
-  );
+  const data = datas
+    .filter(
+      (item) =>
+        item.city.toString().toLowerCase().includes(search) ||
+        item.department.toString().toLowerCase().includes(search) ||
+        item.firstName.toString().toLowerCase().includes(search) ||
+        item.dateOfBirth.toString().toLowerCase().includes(search) ||
+        item.lastName.toString().toLowerCase().includes(search) ||
+        item.startDate.toString().toLowerCase().includes(search) ||
+        item.state.toString().toLowerCase().includes(search) ||
+        item.street.toString().toLowerCase().includes(search) ||
+        item.zipCode.toString().toLowerCase().includes(search)
+    )
+    .slice(
+      numberOfEmployeesVisited,
+      numberOfEmployeesVisited + employeesPerPage
+    );
+
+  const changePage = ({ selected }) => {
+    setPage(selected);
+  };
 
   return (
     <div id="employee-div" className="wrapper">
@@ -97,7 +49,9 @@ function List() {
             Show{" "}
             <select>
               <option value="10">10</option>
-              <option value="20">20</option>
+              <option value="25">25</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
             </select>{" "}
             entries
           </div>
@@ -113,14 +67,21 @@ function List() {
         </div>
         <div className="employee-table--footer">
           <span>
-            Showing {datas.length < 10 ? datas.length : entry} of {datas.length}{" "}
-            entries
+            Showing{" "}
+            {data.length < employeesPerPage ? data.length : employeesPerPage} of{" "}
+            {datas.length} entries
           </span>
-          <div>
-            <button className="previous">Previous</button>
-            <button className="page-number">1</button>
-            <button className="next">Next</button>
-          </div>
+          <ReactPaginate
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={totalPages}
+            onPageChange={changePage}
+            containerClassName={"navigationButtons"}
+            previousLinkClassName={"previousButton"}
+            nextLinkClassName={"nextButton"}
+            disabledClassName={"navigationDisabled"}
+            activeClassName={"navigationActive"}
+          ></ReactPaginate>
         </div>
       </div>
     </div>
